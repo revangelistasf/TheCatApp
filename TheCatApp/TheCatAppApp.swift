34b6a7d7e4fbbb2eadd6627d601874aaf16b0442
Roberto Evangelista <robertoevangelistasf@gmail.com>
2024-05-03 10:24:17 +0100

    Initial Commit

---
+++ TheCatApp/TheCatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheCatAppApp.swift
+//  TheCatApp
+//
+//  Created by revangelista on 03/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct TheCatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
