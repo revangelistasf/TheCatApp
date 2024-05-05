8b2d395c92f913c2f0f48f6b6608d2126f62d422
Roberto Evangelista <robertoevangelistasf@gmail.com>
2024-05-06 00:42:44 +0100

    create BreedDetailView

--- TheCatApp/Scene/Details/BreedDetailsView.swift
+++ TheCatApp/Scene/Details/BreedDetailsView.swift
@@ -9,7 +9,37 @@ import SwiftUI
 
 struct BreedDetailsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ZStack(alignment: .topTrailing) {
+                Image(ImageResource(name: "cat", bundle: .main))
+                    .resizable()
+                    .aspectRatio(1, contentMode: .fill)
+                    .frame(height: 300, alignment: .top)
+                    .clipShape(
+                        .rect(topLeadingRadius: 20, topTrailingRadius: 20)
+                    )
+                FavoriteButton(isFavorite: false, favoriteStyle: .details)
+                    .padding()
+            }
+            VStack(spacing: 16) {
+                Text("Bokolaumius")
+                    .font(.title)
+                    .lineLimit(2)
+                    .foregroundColor(.titleText)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                TitleWithParagraphView(viewModel: .init(title: "Origin", description: "Egypt"))
+                TitleWithParagraphView(viewModel:
+                        .init(title: "Temperament",
+                              description: "Active, Energetic, Independent, Intelligent, Gentle")
+                )
+                TitleWithParagraphView(viewModel:
+                        .init(title: "Description",
+                              description: "The Abyssinian is easy to care for, and a joy to have in your home. They’re affectionate cats and love both people and other animals.")
+                )
+            }
+            .padding(.horizontal)
+            Spacer()
+        }
     }
 }
 
