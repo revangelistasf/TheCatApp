47c949ca8e5b3957de3bbd6aebf6103294a3035b
Roberto Evangelista <robertoevangelistasf@gmail.com>
2024-05-08 13:40:20 +0100

    extend breed to equatable for tests

--- TheCatApp/Model/Breed.swift
+++ TheCatApp/Model/Breed.swift
@@ -54,26 +54,8 @@ class Breed: Decodable {
     }
 }
 
-extension Breed {
-    static func fixture(
-        id: String = "abys",
-        name: String = "Abyssinian",
-        description: String = "The Abyssinian is easy to care for, and a joy to have in your home. They’re affectionate cats and love both people and other animals.",
-        origin: String = "Egypt",
-        temperament: String = "Active, Energetic, Independent, Intelligent, Gentle",
-        referenceImageId: String? = "0XYvRd7oD",
-        lifeSpan: String = "14 - 15",
-        isFavorite: Bool = false
-    ) -> Breed {
-        return Breed(
-            id: id,
-            name: name,
-            description: description,
-            origin: origin,
-            temperament: temperament,
-            referenceImageId: referenceImageId,
-            lifeSpan: lifeSpan,
-            isFavorite: isFavorite
-        )
+extension Breed: Equatable {
+    static func == (lhs: Breed, rhs: Breed) -> Bool {
+        lhs.id == rhs.id
     }
 }
