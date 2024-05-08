0e6e6aabd3e69259d78ab487c42fd9115fd1dca1
Roberto Evangelista <robertoevangelistasf@gmail.com>
2024-05-08 16:55:53 +0100

    create repository tests

--- TheCatAppTests/RepositoryTests.swift
+++ TheCatAppTests/RepositoryTests.swift
@@ -10,50 +10,93 @@ import XCTest
 import CoreData
 
 final class RepositoryTests: XCTestCase {
-    private var mockNetworkService: MockNetworkService!
-    private var mockPersistenceService: MockPersistenceService!
+    private var mockedNetworkService: MockNetworkService!
+    private var mockedPersistenceService: MockPersistenceService!
     private var sut: BreedRepositoryProtocol!
 
     override func setUp() {
         super.setUp()
-        mockNetworkService = MockNetworkService()
-        mockPersistenceService = MockPersistenceService()
-        sut = BreedRepository(networkService: mockNetworkService, persistenceService: mockPersistenceService)
+        mockedNetworkService = MockNetworkService()
+        mockedPersistenceService = MockPersistenceService()
+        sut = BreedRepository(networkService: mockedNetworkService, persistenceService: mockedPersistenceService)
     }
 
     override func tearDown() {
         super.tearDown()
-        mockNetworkService = nil
-        mockPersistenceService = nil
+        mockedNetworkService = nil
+        mockedPersistenceService = nil
         sut = nil
     }
 
     func test_fetchAllFavorites_persistenceReturnSuccess_doesReturnFavorites() {
-        try! mockPersistenceService.addFavorite(breed: .fixture())
-        mockPersistenceService.successRequest = true
+        try! mockedPersistenceService.addFavorite(breed: .fixture())
+        mockedPersistenceService.successRequest = true
         let favorites = try! sut.fetchAllFavorites()
         XCTAssertEqual(favorites, [.fixture()])
     }
 
     func test_fetchAllFavorites_persistenceThrowsError_doesThrowsPersistenceError() {
-        try! mockPersistenceService.addFavorite(breed: .fixture())
-        mockPersistenceService.successRequest = false
+        try! mockedPersistenceService.addFavorite(breed: .fixture())
+        mockedPersistenceService.successRequest = false
         XCTAssertThrowsError(try sut.fetchAllFavorites()) { error in
             XCTAssertEqual(error as! PersistenceError, .failedToFetch)
         }
+    }
+
+    func test_fetchBreeds_doesReturnCorrectResult() {
+        let mockedData = [Breed.fixture()]
+        mockedNetworkService.onRequest = { _ in
+            return mockedData.jsonData()!
+        }
+
+        runAsyncTest {
+            let response = try await self.sut.fetchBreeds(page: 0)
+            XCTAssertEqual(mockedData, response)
+        }
+    }
+
+    func test_fetchBreeds_doesReturnCorrectFavoritedResult() throws {
+        let mockedData = [Breed.fixture()]
+        mockedNetworkService.onRequest = { _ in
+            return mockedData.jsonData()!
+        }
+        try mockedPersistenceService.addFavorite(breed: Breed.fixture())
+
+        runAsyncTest {
+            let response = try await self.sut.fetchBreeds(page: 0)
+            XCTAssertTrue(response.first?.isFavorite ?? false)
+        }
+    }
+
+    func test_toggleFavorite_souldChangePropertyAndAddToDatabase() {
+        let mockedData = Breed.fixture()
+        sut.toggleFavorite(breed: mockedData)
+        XCTAssertEqual(mockedData.isFavorite, true)
+        XCTAssertEqual(mockedPersistenceService.favorites.count, 1)
+        sut.toggleFavorite(breed: mockedData)
+        XCTAssertEqual(mockedPersistenceService.favorites.count, 0)
+    }
+}
+
+extension Array where Element: Encodable {
+    func jsonData() -> Data? {
+        let encoder = JSONEncoder()
+        return try? encoder.encode(self)
     }
 }
 
 extension RepositoryTests {
     class MockNetworkService: NetworkServiceProtocol {
+        var onRequest: ((_ endpoint: Endpoint) async throws -> Data)?
         func request(_ endpoint: Endpoint) async throws -> Data {
-            return Data()
+            guard let request = try await onRequest?(endpoint) else { throw NetworkError.invalidResponse }
+            return request
         }
     }
 
     class MockPersistenceService: BreedPersistenceProtocol {
         var favorites: [BreedPersistenceModel] = []
-        var successRequest: Bool = false
+        var successRequest: Bool = true
 
         func fetchFavorites() throws -> [BreedPersistenceModel] {
             if successRequest {
@@ -69,7 +112,7 @@ extension RepositoryTests {
         }
         
         func removeFavorite(index: String) throws {
-            favorites.remove(at: Int(index)!)
+            favorites.removeAll(where: { $0.id == index} )
         }
 
         lazy var persistentContainer: NSPersistentContainer = {
