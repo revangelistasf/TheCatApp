4d6113b9a6dc980fcca05a395059fe25ba8fb465
Roberto Evangelista <robertoevangelistasf@gmail.com>
2024-05-08 13:36:59 +0100

    add fetch favorites tests

--- TheCatAppTests/Fixtures/Breed+Fixture.swift
+++ TheCatAppTests/Fixtures/Breed+Fixture.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+@testable import TheCatApp
+
+extension Breed {
+    static func fixture(
+        id: String = "abys",
+        name: String = "Abyssinian",
+        description: String = "The Abyssinian is easy to care for, and a joy to have in your home. They’re affectionate cats and love both people and other animals.",
+        origin: String = "Egypt",
+        temperament: String = "Active, Energetic, Independent, Intelligent, Gentle",
+        referenceImageId: String? = "0XYvRd7oD",
+        lifeSpan: String = "14 - 15",
+        isFavorite: Bool = false
+    ) -> Breed {
+        return Breed(
+            id: id,
+            name: name,
+            description: description,
+            origin: origin,
+            temperament: temperament,
+            referenceImageId: referenceImageId,
+            lifeSpan: lifeSpan,
+            isFavorite: isFavorite
+        )
+    }
+}
--- TheCatAppTests/RepositoryTests.swift
+++ TheCatAppTests/RepositoryTests.swift
@@ -6,30 +6,83 @@
 //
 
 import XCTest
+@testable import TheCatApp
+import CoreData
 
-final class TheCatAppTests: XCTestCase {
+final class RepositoryTests: XCTestCase {
+    private var mockNetworkService: MockNetworkService!
+    private var mockPersistenceService: MockPersistenceService!
+    private var sut: BreedRepositoryProtocol!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        super.setUp()
+        mockNetworkService = MockNetworkService()
+        mockPersistenceService = MockPersistenceService()
+        sut = BreedRepository(networkService: mockNetworkService, persistenceService: mockPersistenceService)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        super.tearDown()
+        mockNetworkService = nil
+        mockPersistenceService = nil
+        sut = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func test_fetchAllFavorites_persistenceReturnSuccess_doesReturnFavorites() {
+        try! mockPersistenceService.addFavorite(breed: .fixture())
+        mockPersistenceService.successRequest = true
+        let favorites = try! sut.fetchAllFavorites()
+        XCTAssertEqual(favorites, [.fixture()])
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    func test_fetchAllFavorites_persistenceThrowsError_doesThrowsPersistenceError() {
+        try! mockPersistenceService.addFavorite(breed: .fixture())
+        mockPersistenceService.successRequest = false
+        XCTAssertThrowsError(try sut.fetchAllFavorites()) { error in
+            XCTAssertEqual(error as! PersistenceError, .failedToFetch)
+        }
+    }
+}
+
+extension RepositoryTests {
+    class MockNetworkService: NetworkServiceProtocol {
+        func request(_ endpoint: Endpoint) async throws -> Data {
+            return Data()
         }
     }
 
+    class MockPersistenceService: BreedPersistenceProtocol {
+        var favorites: [BreedPersistenceModel] = []
+        var successRequest: Bool = false
+
+        func fetchFavorites() throws -> [BreedPersistenceModel] {
+            if successRequest {
+                favorites
+            } else {
+                throw PersistenceError.failedToFetch
+            }
+        }
+        
+        func addFavorite(breed: Breed) throws {
+            let persistenceModel = BreedPersistenceModel(breed: breed, context: persistentContainer.viewContext)
+            favorites.append(persistenceModel)
+        }
+        
+        func removeFavorite(index: String) throws {
+            favorites.remove(at: Int(index)!)
+        }
+
+        lazy var persistentContainer: NSPersistentContainer = {
+            let description = NSPersistentStoreDescription()
+            description.url = URL(fileURLWithPath: "/dev/null")
+            let container = NSPersistentContainer(name: "TheCatApp")
+            container.persistentStoreDescriptions = [description]
+            container.loadPersistentStores { _, error in
+                if let error = error as NSError? {
+                    fatalError("Unresolved error \(error), \(error.userInfo)")
+                }
+            }
+            return container
+        }()
+    }
 }
